ac0e97243c76896f05495c8252705a0dec8eb223
Balmukund Patidar <mukunpatidar@gmail.com>
2026-06-10 12:39:58 +0530

    fix sitemap domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://clinik-optimal.vercel.app',
+      url: 'https://www.optimalphysiotherapyclinic.com',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: 'https://clinik-optimal.vercel.app/book',
+      url: 'https://www.optimalphysiotherapyclinic.com/book',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
-      url: 'https://clinik-optimal.vercel.app/gallery',
+      url: 'https://www.optimalphysiotherapyclinic.com/gallery',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
