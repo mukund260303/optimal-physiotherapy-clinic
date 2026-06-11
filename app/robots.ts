5f7e3a9aca24d25fc658f1e7118d9102c4f56e9f
Balmukund Patidar <mukunpatidar@gmail.com>
2026-06-11 15:29:08 +0530

    fix sitemap domain

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next'
- 
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
@@ -7,6 +7,9 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/admin', '/login'],
     },
-    sitemap: 'https://clinik-optimal.vercel.app/sitemap.xml',
+
+    sitemap: 'https://www.optimalphysiotherapyclinic.com/sitemap.xml',
+
+    host: 'https://www.optimalphysiotherapyclinic.com',
   }
 }
